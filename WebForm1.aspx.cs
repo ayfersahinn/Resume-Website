3a7d574a7187de0e10b0e2c02692e200e7a52358
Ayfer Şahin <ayfers547@gmail.com>
2024-09-10 15:14:47 +0300

    Fetched data from the database for about, experience and education

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,7 +11,13 @@ namespace CVBlog
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataSet1TableAdapters.ABOUTTableAdapter dt = new DataSet1TableAdapters.ABOUTTableAdapter();
+            Repeater1.DataSource = dt.aboutList();
+            Repeater1.DataBind();
 
+            DataSet1TableAdapters.EXPERIENCETableAdapter dt2 = new DataSet1TableAdapters.EXPERIENCETableAdapter();
+            Repeater2.DataSource = dt2.ExperienceList();
+            Repeater2.DataBind();
         }
     }
 }
