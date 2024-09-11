2c1fb0973dcfaa1bb3c1c3f32118b941ec722012
Ayfer Şahin <ayfers547@gmail.com>
2024-09-11 21:00:12 +0300

    fetched data from award table

---
+++ adminAward.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVBlog
+{
+    public partial class adminAward : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.AWARDSTableAdapter dt = new DataSet1TableAdapters.AWARDSTableAdapter();
+            Repeater1.DataSource = dt.awardsList();
+            Repeater1.DataBind();
+        }
+    }
+}
