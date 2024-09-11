d9e3948f2029e6f6b6babcf4611019b160964136
Ayfer Şahin <ayfers547@gmail.com>
2024-09-11 19:13:50 +0300

    admin skill deleting is done

---
+++ adminSkillsDelete.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVBlog
+{
+    public partial class adminSkillsDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.SKILLSTableAdapter dt = new DataSet1TableAdapters.SKILLSTableAdapter();
+            int id= Convert.ToInt16(Request.QueryString["ID"]);
+            dt.skillDelete(Convert.ToInt16(id));
+            Response.Redirect("adminSkill.aspx");
+        }
+    }
+}
