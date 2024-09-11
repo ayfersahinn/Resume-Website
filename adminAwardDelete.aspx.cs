cbcb849fecf0aa4c9be01b4bd13e38c9e5e8c7bf
Ayfer Şahin <ayfers547@gmail.com>
2024-09-11 21:08:30 +0300

    admin award deleting is done

---
+++ adminAwardDelete.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVBlog
+{
+    public partial class adminAwardDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.AWARDSTableAdapter dt = new DataSet1TableAdapters.AWARDSTableAdapter();
+            int id = Convert.ToInt16(Request.QueryString["ID"]);
+            dt.awardDelete(Convert.ToInt16(id));
+            Response.Redirect("adminAward.aspx");
+        }
+    }
+}
