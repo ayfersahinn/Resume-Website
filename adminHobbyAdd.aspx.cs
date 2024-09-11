48ce34342e1d08965d5d98c3230ce747a582ca09
Ayfer Şahin <ayfers547@gmail.com>
2024-09-11 20:45:52 +0300

    admin hobby adding is done

---
+++ adminHobbyAdd.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CVBlog
+{
+    public partial class adminHobbyAdd : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.HOBBYTableAdapter dt = new DataSet1TableAdapters.HOBBYTableAdapter();
+            dt.hobbyAdd(txtHobby.Text);
+            Response.Redirect("adminHobby.aspx");
+        }
+    }
+}
